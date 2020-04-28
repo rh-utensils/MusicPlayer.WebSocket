f8e93909f3e5d1089037d0484dc3772592634a86
Rene Hampölz <service@hampoelz.net>
2020-04-28 13:56:52 +0200

    Update server.js

--- server.js
+++ server.js
@@ -7,15 +7,65 @@ const PORT = process.env.PORT || 3000;
 const INDEX = '/index.html';
 
 const server = express()
-  .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
+  .use((_, res) => res.sendFile(INDEX, { root: __dirname }))
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
 
+// Create and configure socket.io 
 const io = socketIO(server);
-io.origins(['https://music.hampoelz.net']);
+//io.origins(['https://music.hampoelz.net']);
+
+// keeping track of connections
+var sockets = {};
 
 io.on('connection', (socket) => {
   console.log('Client connected');
-  socket.on('disconnect', () => console.log('Client disconnected'));
+
+  var id;
+
+	// Fetermine an identifier that is unique for us.
+
+	do {
+		id = uuid.v4();
+	} while (sockets[id]);
+
+	// We have a unique identifier that can be sent to the client
+
+	sockets[id] = socket;
+	socket.emit('room-id', id);
+
+	// Remove references to the disconnected socket
+	socket.on('disconnect', () => {
+		sockets[socket] = undefined;
+		delete sockets[socket];
+
+		console.log('Client disconnected')
+	});
+
+	// When a message is received forward it to the addressee
+	socket.on('message', (message) => {
+		if (sockets[message.to]) {
+			sockets[message.to].emit('message', message);
+		} else {
+			socket.emit('disconnected', message.from);
+		}
+	});
+
+	// When a listener logs on let the media streaming know about it
+	socket.on('logon', (message) => {
+		if (sockets[message.to]) {
+			sockets[message.to].emit('logon', message);
+		} else {
+			socket.emit('error', 'Does not exsist at server.');
+		}
+	});
+
+	socket.on('logoff', (message) => {
+		if (sockets[message.to]) {
+			sockets[message.to].emit('logoff', message);
+		} else {
+			socket.emit('error', 'Does not exsist at server.');
+		}
+	});
 });
 
 setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
